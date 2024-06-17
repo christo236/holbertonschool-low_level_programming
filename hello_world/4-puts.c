8fbd3414dd5be84e816314c5c5504cd17c47ab20
christo236 <christopherkoisse@icloud.com>
2024-06-17 10:04:47 +0000

    question 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -3,7 +3,9 @@
 /**
  * main - hello,puts 
  *
- * Description : Print exactly "Programming is like building a multilingual puzzle, followed by a new line.
+ * Description : Print exactly "Programming is like
+ * building a multilingual
+ * puzzle, followed by a new line.
  *
  * Return: Always 0.
  */
