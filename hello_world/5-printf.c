6541f5f5ecf87e023274ab1c0b408b3df86b58ea
christo236 <christopherkoisse@icloud.com>
2024-06-17 12:58:48 +0000

    question 5

---
+++ hello_world/5-printf.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Hello, printf
+ *
+ * Description: Prints exactly with printf "with proper grammar, but the ouctome is a 
+ * piece of art, followed by a new life.
+ *
+ * Returns Always 0.
+ */
+
+int main(void)
+
+{ 
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0)
+
+}
