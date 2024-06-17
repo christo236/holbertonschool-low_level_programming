0dba760c7e955c656d4cbccafb30632238ad5be9
christo236 <christopherkoisse@icloud.com>
2024-06-17 12:30:29 +0000

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -3,13 +3,15 @@
 /**
  * main - Hello,puts
  *
- * Description : Print exactly "Programming is like building a multilingual puzzle, followed by a new line.
+ * Description : Print exactly "Programming is like building
+ * a multilingual puzzle, followed by a new line.
  *
  * Return: Always 0.
  */
 
-int main (void)
+int main(void)
 {
-puts ("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
 }
