8f8515484f142dbe5b6b537e0b7226477b3d495f
christo236 <christopherkoisse@icloud.com>
2024-06-17 13:29:18 +0000

    question 5

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -3,13 +3,13 @@
 /**
  * main - Hello, printf
  *
- * Description: Prints exactly with printf "with proper grammar, but the outcome is a 
- * piece of art, followed by a new life.
+ * Description: Prints exactly with printf "with proper grammar,
+ * but the outcome is a piece of art,
  * Returns: Always 0.
  */
 
 int main(void)
-{ 
-	printf("with proper grammar, but the outcome is a piece of art, \n");
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
