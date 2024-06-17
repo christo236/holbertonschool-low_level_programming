6eaf09349f8788840da5295fe8b5bb678507ea30
christo236 <christopherkoisse@icloud.com>
2024-06-17 12:24:22 +0000

    question 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 
 /**
- * main - Entry point 
+ * main - Hello,puts
  *
- * Return: Always 0
+ * Description : Print exactly "Programming is like building a multilingual puzzle, followed by a new line.
+ *
+ * Return: Always 0.
  */
-int main(void)
+
+int main (void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0)
+puts ("\"Programming is like building a multilingual puzzle");
+return (0);
 }
