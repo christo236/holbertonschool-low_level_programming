3d6f45d13100ce882ee10a02299915bed598f8c0
christo236 <christopherkoisse@icloud.com>
2024-06-18 09:05:33 +0000

    question 6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
 
-int main(void) {
-    printf("Taille de char: %lu octet(s)\n", sizeof(char));
-    printf("Taille de int: %lu octet(s)\n", sizeof(int));
-    printf("Taille de long int: %lu octet(s)\n", sizeof(long int));
-    printf("Taille de long long int: %lu octet(s)\n", sizeof(long long int));
-    printf("Taille de float: %lu octet(s)\n", sizeof(float));
-    printf("Taille de double: %lu octet(s)\n", sizeof(double));
-    printf("Taille de long double: %lu octet(s)\n", sizeof(long double));
-    return 0;
+/**
+ * main - Size is not grandeur, and territory does not make a nation
+ *
+ * Description: prints the size of various types
+ *              on the computer it is compiled ans run on.
+ * Returns: always 0
+ * /
+
+int main(void)
+{
+printf("Taille de char: %lu octet(s)\n", sizeof(char));
+printf("Taille de int: %lu octet(s)\n", sizeof(int));
+printf("Taille de long int: %lu octet(s)\n", sizeof(long int));
+printf("Taille de long long int: %lu octet(s)\n", sizeof(long long int));
+printf("Taille de float: %lu octet(s)\n", sizeof(float));
+printf("Taille de double: %lu octet(s)\n", sizeof(double));
+printf("Taille de long double: %lu octet(s)\n", sizeof(long double));
+
+return 0;
 }
