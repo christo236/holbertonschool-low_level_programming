689bab9ae5760f8d31f675ed5128ae638897b3ef
christo236 <christopherkoisse@icloud.com>
2024-06-19 13:26:11 +0000

    question 2

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -15,12 +15,12 @@ int main(void)
 {
 char alphabet;
 
-for (alphabet - 'a'; alphabet <= 'z'; alphabet++)
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 {
 putchar(alphabet);
 }
 
-for (alphabet - 'A'; alphabet <= 'Z'; alphabet++)
+for (alphabet = 'A'; alphabet <= 'Z'; alphabet++)
 {
 putchar(alphabet);
 }
