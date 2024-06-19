5f11aa4de4fa33fe6455733285755359f5f9a123
christo236 <christopherkoisse@icloud.com>
2024-06-19 12:55:36 +0000

    question 2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - The alphabet game
+ *
+ * print alphabet en minuscule avec new lign
+ *
+ * Return: Always 0.
+ *
+ */
+
+int main(void)
+{
+char alphabet;
+
+for (alphabet - 'a'; alphabet <= 'z'; alphabet++)
+{
+putchar(alphabet);
+}
+
+for (alphabet - 'A'; alphabet <= 'Z'; alphabet++)
+{
+putchar(alphabet);
+}
+putchar('\n');
+return (0);
+
+}
