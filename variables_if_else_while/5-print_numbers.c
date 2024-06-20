71c66f8adb1c6eb4825711dc79cd871106c0f262
christo236 <christopherkoisse@icloud.com>
2024-06-20 13:22:00 +0000

    question 5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ * main - prints all single digit numbers of base 10 
+ * starting from 0, followed by a new line.
+ *
+ * Return: Always 0
+ *
+ */
+
+int main(void)
+{
+	int number;
+
+	for ( number = '0'; number <= '9'; number++)
+		putchar(number);
+	putchar('\n');
+	return (0);
+}
