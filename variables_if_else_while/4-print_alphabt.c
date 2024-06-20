5a88d7867bee1d696ca46b2ec5079a8a3775328b
christo236 <christopherkoisse@icloud.com>
2024-06-20 12:24:24 +0000

    question 4

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase, followed by a new line.
+ *
+ * Return: Always 0
+ *
+ */
+
+int main(void)
+{
+	int alphab;
+
+	for (alphab = 'a'; alphab <= 'z'; alphab++)
+		if (alphab == 'q' || alphab == 'e')
+			alphab++;
+		else
+			putchar(alphab);
+putchar('\n');
+return (0);
+}
