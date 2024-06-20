fc39d1b5b8634814abaa6310549275d43791c80c
christo236 <christopherkoisse@icloud.com>
2024-06-20 14:01:51 +0000

    question 7

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ * main - prints the lowercase alphabet in reverse,
+ * followed by a new line.
+ *
+ * Return: Always 0
+ *
+ */
+
+int main(void)
+{
+	int miror;
+
+	for (miror = 'z'; miror <= 'a'; miror--)
+		putchar(miror);
+	putchar('\n');
+	return (0);
+}
