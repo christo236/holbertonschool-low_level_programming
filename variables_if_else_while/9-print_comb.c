05439ec81bc91e2bab78987aaa3e06122a8f782b
christo236 <christopherkoisse@icloud.com>
2024-06-21 08:44:42 +0000

    question 9

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 
 /**
- * prints all possible combinations
+ * main - prints all possible combinations
  * of single-digit numbers.
  *
  * Return: Always 0
@@ -13,10 +13,14 @@ int main(void)
 	int x;
 
 	for (x = '0'; x <= '9'; x++)
+	{
 		putchar ('0' + x);
 		if (x != '9')
+		{
 			putchar(',');
 			putchar(' ');
+		}
+	}
 putchar('\n');
 return (0);
 }
