05a042fa030cd386b5588cefb99283b374735cdf
christo236 <christopherkoisse@icloud.com>
2024-06-21 10:14:26 +0000

    question 0

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+ * main - prints pputchar, followed by a new line.
+ *
+ * Return: Always 0
+ *
+ */
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
